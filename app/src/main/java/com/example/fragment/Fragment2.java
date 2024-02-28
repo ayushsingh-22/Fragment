1f26978884c179d8d180bea08721aad644a98503
ayushsingh-22 <ayushkumar2205@gmail.com>
2024-02-28 21:26:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragment/Fragment2.java
@@ -0,0 +1,29 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragment2 extends Fragment {
+
+
+
+    public Fragment2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+        TextView txtview = view.findViewById(R.id.txt2);
+        return view;
+    }
+}
